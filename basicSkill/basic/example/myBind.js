960be27f4404ecffa5535bd8755d07c76a667cc5
jingws <632397482@qq.com>
2020-05-05 14:24:17 +0800

    bind

---
+++ basicSkill/basic/example/myBind.js
@@ -0,0 +1,16 @@
+Function.prototype.myBind = function(thisArg, ...args) {
+  const func = this
+
+  // bind 返回的是一个新函数
+  return function(...otherArg) {
+    // 执行函数时 this 始终为外层函数中的 thisArg，前面的调用参数也被绑定为 args
+    return func.call(thisArg, ...args, ...otherArg)
+  }
+}
+
+function testMyBind() {
+  console.log(`This is ${JSON.stringify(this)}, arguments is ${[...arguments].join(', ')}`)
+}
+
+const boundFunc = testMyBind.myBind({name: 'jws'}, 1, 2, 3)
+boundFunc(4, 5, 6) // This is {"name":"jws"}, arguments is 1, 2, 3, 4, 5, 6
